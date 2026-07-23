5c6aa11470451861b87f0b8b395f03669e0f01ad
Михаил <163150274+misha-korn@users.noreply.github.com>
2026-07-23 17:23:43 +0400

    Перенастроил окружение контейнера

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,13 @@ ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y --no-install-recommends \
-    gcc libpq-dev && rm -rf /var/lib/apt/lists/*
+RUN apt-get update  \
+    && apt-get install -y --no-install-recommends \
+    gcc \
+    libpq-dev \
+    gettext \
+    vim \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN pip install --no-cache-dir pipenv
 COPY Pipfile Pipfile.lock ./
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 set -e
+
+: "${DB_HOST:?DB_HOST is required}"
+: "${DB_PORT:?DB_PORT is required}"
+
 echo "Ждём базу..."
-until python -c "import socket; socket.create_connection(('db', 5432), timeout=2)" 2>/dev/null; do
-  sleep 1
+until python -c "import socket, os; socket.create_connection((os.environ['DB_HOST'], int(os.environ['DB_PORT'])), timeout=2)" 2>/dev/null;
+do
+    sleep 1
 done
 
-if [ "$RUN_MIGRATIONS" = "1" ]; then       # ← только там, где флаг
+if [ "$RUN_MIGRATIONS" = "1" ];
+then
   python manage.py migrate --noinput
 fi
 
-if [ "$COLLECTSTATIC" = "1" ]; then
+if [ "$COLLECTSTATIC" = "1" ];
+then
   python manage.py collectstatic --noinput
 fi
 
